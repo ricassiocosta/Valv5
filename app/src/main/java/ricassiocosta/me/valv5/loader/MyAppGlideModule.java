d983027058bd53893fbf0c48b40b4bbfc7b3545d
ricassio costa <ricassiocosta.me@gmail.com>
2025-12-03 16:22:54 -0300

    Enhance security by preventing disk writes for encrypted/decrypted data in Glide

--- app/src/main/java/ricassiocosta/me/valv5/loader/MyAppGlideModule.java
+++ app/src/main/java/ricassiocosta/me/valv5/loader/MyAppGlideModule.java
@@ -31,9 +31,17 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.module.AppGlideModule;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.io.File;
 import java.io.InputStream;
 
 import ricassiocosta.me.valv5.data.EncryptedFile;
+import com.bumptech.glide.load.Options;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.resource.drawable.DrawableResource;
+import com.bumptech.glide.load.resource.bitmap.BitmapResource;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import com.bumptech.glide.load.Encoder;
 
 @GlideModule
 public class MyAppGlideModule extends AppGlideModule {
@@ -41,14 +49,33 @@ public class MyAppGlideModule extends AppGlideModule {
     @Override
     public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
         registry.prepend(EncryptedFile.class, InputStream.class, new EncryptedFileModelLoaderFactory(context));
+        
+        // Prevent any encrypted/decrypted data from being written to disk cache
+        // This ensures security by rejecting ALL disk write attempts
+        registry.prepend(Bitmap.class, new Encoder<Bitmap>() {
+            @Override
+            public boolean encode(@NonNull Bitmap data, @NonNull File file, @NonNull Options options) {
+                return false; // Reject all disk writes
+            }
+        });
+        
+        registry.prepend(Drawable.class, new Encoder<Drawable>() {
+            @Override
+            public boolean encode(@NonNull Drawable data, @NonNull File file, @NonNull Options options) {
+                return false; // Reject all disk writes
+            }
+        });
     }
 
     @Override
     public void applyOptions(@NonNull Context context, @NonNull GlideBuilder builder) {
         builder.setLogLevel(Log.ERROR);
         // Disable disk cache globally for security - no decrypted data should be written to disk
+        // Also skip memory cache to ensure no cached data persists
         builder.setDefaultRequestOptions(
-                new RequestOptions().diskCacheStrategy(DiskCacheStrategy.NONE)
+                new RequestOptions()
+                        .diskCacheStrategy(DiskCacheStrategy.NONE)
+                        .skipMemoryCache(true)
         );
         super.applyOptions(context, builder);
     }
