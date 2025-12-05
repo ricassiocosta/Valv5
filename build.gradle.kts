050f16ac1a8d6767c1d66c768a53dff205f15764
ricassio costa <ricassiocosta.me@gmail.com>
2025-12-05 14:13:12 -0300

    Chore: Update dependencies and Gradle wrapper to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.about.libraries) apply false
     alias(libs.plugins.kotlin.android) apply false
